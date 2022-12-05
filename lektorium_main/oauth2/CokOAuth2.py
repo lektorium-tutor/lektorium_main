1d2a6cceed523948ad824721da3a19a2042852ec
RaccoonBoyG <alexofficialkek@gmail.com>
2022-12-05 18:15:50 +0500

    cok auth test

--- lektorium_main/oauth2/CokOAuth2.py
+++ lektorium_main/oauth2/CokOAuth2.py
@@ -53,11 +53,12 @@ import json
 import logging
 from urllib.parse import urljoin
 
+
 class CokOAuth2(BaseOAuth2):
     name = 'educont'
-    API_URL='https://dev.educont.ru/api/external/v1/'
-    AUTHORIZATION_URL = 'https://dev.educont.ru/oauth/authorize'  # '{}/oauth2/authorize'.format(settings.SSO_ROO_URL)
-    ACCESS_TOKEN_URL = 'https://dev.educont.ru/api/external/v1/oauth/token'  # '{}/oauth2/access_token'.format(settings.SSO_ROO_URL)
+    API_URL = 'https://sso.preprod.educont.ru/api/external/v1/'
+    AUTHORIZATION_URL = 'https://sso.preprod.educont.ru/oauth/authorize'  # '{}/oauth2/authorize'.format(settings.SSO_ROO_URL)
+    ACCESS_TOKEN_URL = 'https://sso.preprod.educont.ru/api/external/v1/oauth/token'  # '{}/oauth2/access_token'.format(settings.SSO_ROO_URL)
     DEFAULT_SCOPE = ["external_system.read"]
     ACCESS_TOKEN_METHOD = 'POST'
     REDIRECT_STATE = False
@@ -82,6 +83,12 @@ class CokOAuth2(BaseOAuth2):
             middleName=response.get('middleName')
         )
         email = response.get('email')
+        logging.warning({
+            'username': nickname,
+            'email': email,
+            'fullname': fullname,
+            'first_name': name,
+            'last_name': surname})
         return {
             'username': nickname,
             'email': email,
@@ -90,35 +97,15 @@ class CokOAuth2(BaseOAuth2):
             'last_name': surname
         }
 
+    # def _user_data(self, access_token, path=None):
+    #     url = urljoin(self.api_url(), 'profile')
+    #     logging.warning(self.get_json(url, method="POST", headers={'Authorization': f'token {access_token}'}))
+    #     return self.get_json(url, method="POST", headers={'Authorization': f'token {access_token}'})
+
     def user_data(self, access_token, *args, **kwargs):
-        """Loads user data from service"""
-        data = self._user_data(access_token)
-        # if not data.get('email'):
-        #     try:
-        #         emails = self._user_data(access_token, '/emails')
-        #     except (HTTPError, ValueError, TypeError):
-        #         emails = []
-
-        #     if emails:
-        #         email = emails[0]
-        #         primary_emails = [
-        #             e for e in emails
-        #             if not isinstance(e, dict) or e.get('primary')
-        #         ]
-        #         if primary_emails:
-        #             email = primary_emails[0]
-        #         if isinstance(email, dict):
-        #             email = email.get('email', '')
-        #         data['email'] = email
-        return data
-
-    def _user_data(self, access_token, path=None):
-        url = urljoin(self.api_url(), 'profile')
-        logging.warning(self.get_json(url, method="POST", headers={'Authorization': f'token {access_token}'}))
-        return self.get_json(url, method="POST", headers={'Authorization': f'token {access_token}'})
-
-    # def user_data(self, access_token, *args, **kwargs):
-    #     return self.get_json('https://dev.educont.ru/api/external/v1/profile',method="POST" , headers={
-    #         'Authorization': '{0} {1}'.format(response.get('token_type'),
-    #                                           access_token)
-    #     })
+        logging.warning(kwargs.pop('response'))
+        response = kwargs.pop('response')
+        return self.get_json('https://sso.preprod.educont.ru/api/external/v1/profile', method="POST", headers={
+            'Authorization': '{0} {1}'.format(response.get('token_type'),
+                                              access_token)
+        })
