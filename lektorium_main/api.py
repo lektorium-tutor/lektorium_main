29dfe8c0d9a0fcdb9032f9f7fb01080d2f814dcc
RaccoonBoyG <alexofficialkek@gmail.com>
2022-12-23 17:04:19 +0500

    up

--- lektorium_main/api.py
+++ lektorium_main/api.py
@@ -12,7 +12,7 @@ from django.shortcuts import get_object_or_404
 from ninja import NinjaAPI, ModelSchema
 from ninja.orm import create_schema
 from ninja.security import django_auth, HttpBearer
-from datetime import datetime, timezone as timezone_datetime
+import datetime
 from django.utils import timezone
 
 from lektorium_main.profile.models import Profile
@@ -106,14 +106,16 @@ UserProfileSchema = create_schema(
 #         "success": True
 #     }
 
-@api.get("/me", auth=django_auth, response={200: UserProfileSchema, 200: None})
+@api.get("/me", auth=django_auth, response={200: UserProfileSchema, 404: str})
 def me(request):
     user = get_object_or_404(User, username=request.auth)
     profile = Profile.get_polymorph_profile(user)
     if profile:
-        return profile
+        return 200, profile
     else:
-        return 200, None
+        return 404, ""
+    # except:
+    #     return ""
 
 
 @api.delete("/profiles/{profile_id}", auth=django_auth)
@@ -168,14 +170,14 @@ def genToken(request):
 
 def utcformat(dt, timespec='milliseconds'):
     """convert datetime to string in UTC format (YYYY-mm-ddTHH:MM:SS.mmmZ)"""
-    iso_str = dt.astimezone(timezone_datetime.utc).isoformat('T', timespec)
+    iso_str = dt.astimezone(datetime.timezone.utc).isoformat('T', timespec)
     return iso_str.replace('+00:00', 'Z')
 
 @api.post('/feedback', auth=django_auth)
 def feedback(request):
     # try:
-    now = utcformat(datetime.now(tz=timezone_datetime.utc))
-    logging.warning(timezone_datetime.now())
+    now = utcformat(datetime.datetime.now(tz=datetime.timezone.utc))
+    logging.warning(now)
     body = json.loads(request.body.decode())
     path = 'https://api.dev.educont.ru/api/v1/public/educational-courses/feedback'
     method = "POST"
