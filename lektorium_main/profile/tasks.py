e0c2b703bc3e07e666ca702b08ea07f7feeb7029
MasterGowen <mastergowen@gmail.com>
2022-12-26 11:47:12 +0500

    listen_educont_sse task on startup

--- lektorium_main/profile/tasks.py
+++ lektorium_main/profile/tasks.py
@@ -7,11 +7,14 @@ import requests
 from celery import shared_task
 from django.conf import settings
 from django.utils import timezone
+from celery.signals import worker_ready
+
+
 
 logger = logging.getLogger('lektorium_main.profile.tasks')
 
 
-@shared_task
+@shared_task(name='listen_sse')
 def listen_educont_sse():
     request_path = f"{settings.EDUCONT_BASE_URL}/sse/connect"
     request_hash = hashlib.md5(request_path.encode()).hexdigest()
@@ -28,3 +31,8 @@ def listen_educont_sse():
             decoded_line = line.decode('utf-8')
             logger.warning(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!! {json.loads(decoded_line)}')
 
+
+@worker_ready.connect
+def at_start(sender, **k):
+    with sender.app.connection() as conn:
+         sender.app.send_task('listen_sse')
