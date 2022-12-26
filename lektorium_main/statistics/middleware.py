90b306e2e58f7755b7cb57746684d22b7b59fa03
MasterGowen <mastergowen@gmail.com>
2022-12-26 06:03:46 +0500

    fix statistics middleware

--- lektorium_main/statistics/middleware.py
+++ lektorium_main/statistics/middleware.py
@@ -15,6 +15,7 @@ class EducontStatisticsMiddleware(MiddlewareMixin):
 
     def _get_profile(self, request):
         try:
+            logger.warning(f'User: {request.user}, profile: {request.user.verified_profile_educont.first()}')
             if request.user.is_active:
                 return request.user.verified_profile_educont.first()
         except AttributeError:
@@ -22,6 +23,7 @@ class EducontStatisticsMiddleware(MiddlewareMixin):
 
     def _get_view_name(self, request):
         try:
+            logger.warning(f'View name: {request.view_name}')
             return request.view_name
         except AttributeError:
             return None
@@ -42,8 +44,12 @@ class EducontStatisticsMiddleware(MiddlewareMixin):
 
     def process_response(self, request, response):
         profile = self._get_profile(request)
+
+        if not profile:
+            return response
+
         view_name = self._get_view_name(request)
-        if view_name == 'CourseHomeMetadataView':
+        if view_name == 'CourseHomeMetadataView':  # TODO: move to _write_stats
             course = COK.objects.get(course_id=request.path.split('/')[-1])
             EducontStatisticsItem.objects.create(
                 statisticType='s',
