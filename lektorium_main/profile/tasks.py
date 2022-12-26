a735bbfe5c1882687c5901bd6153e332c35a5f7f
MasterGowen <mastergowen@gmail.com>
2022-12-26 11:41:57 +0500

    listen_educont_sse task

---
+++ lektorium_main/profile/tasks.py
@@ -0,0 +1,30 @@
+import hashlib
+import logging
+import json
+
+import jwt
+import requests
+from celery import shared_task
+from django.conf import settings
+from django.utils import timezone
+
+logger = logging.getLogger('lektorium_main.profile.tasks')
+
+
+@shared_task
+def listen_educont_sse():
+    request_path = f"{settings.EDUCONT_BASE_URL}/sse/connect"
+    request_hash = hashlib.md5(request_path.encode()).hexdigest()
+    encoded_token = jwt.encode({
+        "systemName": "Лекториум",
+        "createdTimestamp": int(timezone.now().timestamp()),
+        "requestHash": request_hash,
+        "systemCode": settings.SYSTEM_CODE_EDUCONT
+    }, settings.PRIVATE_KEY_EDUCONT, algorithm="RS256")
+
+    r = requests.get(request_path, stream=True)
+    for line in r.iter_lines():
+        if line:
+            decoded_line = line.decode('utf-8')
+            logger.warning(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!! {json.loads(decoded_line)}')
+
