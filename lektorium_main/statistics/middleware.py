093094bd402fa8314d1cce88a50d8600a177ebc6
MasterGowen <mastergowen@gmail.com>
2022-12-26 07:33:27 +0500

    BlockCompletion

--- lektorium_main/statistics/middleware.py
+++ lektorium_main/statistics/middleware.py
@@ -2,13 +2,14 @@ import logging
 
 from lektorium_main.courses.models import COK, Course
 from .models import EducontStatisticsItem
+from completion.models import BlockCompletion
 
 try:
     from django.utils.deprecation import MiddlewareMixin
 except ImportError:
     MiddlewareMixin = object
 
-logger = logging.getLogger(__file__)
+logger = logging.getLogger('EducontStatisticsMiddleware')
 
 
 class EducontStatisticsMiddleware(MiddlewareMixin):
@@ -28,13 +29,14 @@ class EducontStatisticsMiddleware(MiddlewareMixin):
 
     def _get_view_name(self, request):
         try:
-            logger.warning(f'View name: {request.view_name}')
+            logger.warning(f'View name:from completion.models import BlockCompletion {request.view_name}')
             return request.view_name
         except AttributeError:
             return None
 
     def _write_stats(self, profile, externalId):
         content = Course.objects.get(externalId=externalId)
+        logger.warning(f'!!!!!!!!!!! BloclCompletion: {BlockCompletion.objects.filter(user=profile.user, block_key=content.block_key)}')
         EducontStatisticsItem.objects.create(
             statisticType='s',
             externalId=content.externalId,
@@ -68,6 +70,7 @@ class EducontStatisticsMiddleware(MiddlewareMixin):
             except Course.DoesNotExist:
                 logger.warning(f'EDUCONT content with externalId={request.path.split("@")[-1]} does not exist')
             else:
+
                 self._write_stats(profile, content.externalId)
 
         return response
