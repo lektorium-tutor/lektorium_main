69d1e4765777c9f69d75ecafca00b08bb783e39b
MasterGowen <mastergowen@gmail.com>
2022-12-26 14:24:52 +0500

    revert async task

--- lektorium_main/profile/tasks.py
+++ lektorium_main/profile/tasks.py
@@ -7,20 +7,19 @@ import requests
 from celery import shared_task
 from django.conf import settings
 from django.utils import timezone
-from aiohttp_sse_client import client as sse_client
-import asyncio
-from openedx.core.lib.celery import APP
-import celery_pool_asyncio
+
 # from celery.signals import worker_ready
 
 
 logger = logging.getLogger('lektorium_main.profile.tasks')
 
-app=APP
 
-
-@app.task
-async def listen_educont_sse(*args, **kwargs):
+@shared_task(
+    name='listen_sse',
+    bind=True,
+    acks_late=False,
+)
+def listen_educont_sse(*args, **kwargs):
     logger.warning(f'!!!!!!!!!!!!!! {args}; {kwargs}')
     request_path = f"{settings.EDUCONT_BASE_URL}/api/v1/public/sse/connect"
     request_hash = hashlib.md5(request_path.encode()).hexdigest()
@@ -31,24 +30,12 @@ async def listen_educont_sse(*args, **kwargs):
         "systemCode": settings.SYSTEM_CODE_EDUCONT
     }, settings.PRIVATE_KEY_EDUCONT, algorithm="RS256")
 
-    # r = requests.get(request_path, headers={"Authorization": f"Bearer {encoded_token}"}, stream=True)
-    async with sse_client.EventSource(
-        request_path,
-        headers={"Authorization": f"Bearer {encoded_token}"}
-    ) as event_source:
-        # try:
-        async for event in event_source:
-            logger.warning(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!! {event}')
-        # except ConnectionError:
-        #     pass
-
-
-    # logger.warning(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!! {r}')
-    # for line in r.iter_lines():
-    #     logger.warning(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!! {r.url} -- {line}')
-    #     if line:
-    #         decoded_line = line.decode('utf-8')
-    #         logger.warning(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!! {json.loads(decoded_line)}')
+    r = requests.get(request_path, headers={"Authorization": f"Bearer {encoded_token}"}, stream=True)
+    for line in r.iter_lines():
+        logger.warning(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!! {r.url} -- {line}')
+        if line:
+            decoded_line = line.decode('utf-8')
+            logger.warning(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!! {json.loads(decoded_line)}')
 
 # @worker_ready.connect
 # def at_start(sender, **k):
